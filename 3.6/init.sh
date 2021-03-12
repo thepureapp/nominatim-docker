9a328290b853cb57a85339f6d1eedf30ef56f7d4
DM <asura.hack@gmail.com>
2021-03-12 12:11:27 +0300

    Add aws-cli support. Add PBF file download support. Change default import style to administrative boundaries.

--- 3.6/init.sh
+++ 3.6/init.sh
@@ -1,21 +1,44 @@
-OSMFILE=$1
-PGDIR=$2
-THREADS=$3
+#!/bin/bash
 
-rm -rf /data/$PGDIR && \
-mkdir -p /data/$PGDIR && \
+OSMFILE=${OSMFILE:=osm-file.osm.pbf}
+BACKUPFILE=${BACKUPFILE:=pg-nominatim.tar.gz}
+S3BACKUPFILE=${S3BACKUPFILE:=s3://soul-nominatim/$BACKUPFILE}
+PGDIR=${PGDIR:=postgresqldata}
+THREADS=${THREADS:=4}
+SETUP_PHP_ARGS=${SETUP_PHP_ARGS:="--all --threads $THREADS"}
 
-chown postgres:postgres /data/$PGDIR && \
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
 
-export  PGDATA=/data/$PGDIR  && \
-sudo -u postgres /usr/lib/postgresql/12/bin/initdb -D /data/$PGDIR && \
-sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/$PGDIR start && \
-sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
-sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
-sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
-useradd -m -p password1234 nominatim && \
-chown -R nominatim:nominatim ./src && \
-sudo -u nominatim ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
-sudo -u nominatim ./src/build/utils/check_import_finished.php && \
-sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/$PGDIR stop && \
-sudo chown -R postgres:postgres /data/$PGDIR
+[[ $FORCE_NEW == "yes" ]] && rm -rf /data/$PGDIR
+
+if [ ! -d /data/$PGDIR ] && aws s3 ls $S3BACKUPFILE > /dev/null; then
+    aws s3 cp $S3BACKUPFILE /data/$BACKUPFILE
+    mkdir -p /data/$PGDIR
+    echo "Extracting /data/$BACKUPFILE to /data/$PGDIR..."
+    tar -C /data/$PGDIR -zxf /data/$BACKUPFILE && echo "Done"
+    chown -R postgres:postgres /data/$PGDIR
+    rm /data/$BACKUPFILE
+fi
+
+if [ ! -d /data/$PGDIR ]; then
+    [[ -n $PBF_URL && ! -f /data/$OSMFILE ]] && curl -L $PBF_URL --create-dirs -o /data/$OSMFILE
+    mkdir -p /data/$PGDIR && chown postgres:postgres /data/$PGDIR
+    export  PGDATA=/data/$PGDIR
+    sudo -u postgres /usr/lib/postgresql/12/bin/initdb -D /data/$PGDIR
+    sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/$PGDIR start
+    sudo -u postgres createuser -sD nominatim
+    sudo -u postgres createuser -SDR www-data
+    useradd -m -p password1234 nominatim && chown -R nominatim:nominatim ./src
+    sudo -u nominatim ./src/build/utils/setup.php --osm-file /data/$OSMFILE $SETUP_PHP_ARGS && \
+    sudo -u nominatim ./src/build/utils/check_import_finished.php || exit 1
+    sudo -u nominatim ./src/build/utils/setup.php --drop
+    sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/$PGDIR stop
+    chown -R postgres:postgres /data/$PGDIR
+    [[ -n $PBF_URL ]] && rm /data/$OSMFILE
+    echo "Making S3 backup /data/$PGDIR to $S3BACKUPFILE..."
+    tar -C /data/$PGDIR -zcf /data/$BACKUPFILE . && echo "TAR.GZ archive done to /data/$BACKUPFILE"
+    aws s3 cp /data/$BACKUPFILE $S3BACKUPFILE
+    rm /data/$BACKUPFILE
+fi
--- 3.6/local.php
+++ 3.6/local.php
@@ -1,10 +1,12 @@
 <?php
+// @define('CONST_Import_Style', CONST_BasePath.'/settings/import-full.style'); // default
+ @define('CONST_Import_Style', CONST_BasePath.'/settings/import-admin.style');
  // Paths
  @define('CONST_Postgresql_Version', '12');
  @define('CONST_Postgis_Version', '3');
  // Website settings
  @define('CONST_Website_BaseURL', '/');
- @define('CONST_Replication_Url', 'http://download.geofabrik.de/europe/monaco-updates');
+ // @define('CONST_Replication_Url', 'http://download.geofabrik.de/europe/monaco-updates');
  @define('CONST_Replication_MaxInterval', '86400');     // Process each update separately, osmosis cannot merge multiple updates
  @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
  @define('CONST_Replication_Recheck_Interval', '900');   // How long to sleep if no update found yet
